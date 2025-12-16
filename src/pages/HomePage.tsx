50e93ef872f717f18502f0e90f552346b5b7ab5d
mmD8984 <21130341@st.hcmuaf.edu.vn>
2025-12-16 17:47:42 +0700

    setup router

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+const HomePage = () => {
+    return(
+        <p> hello homepages</p>
+    )
+}
+
+export default HomePage
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,14 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+const HomePage = lazy(() => import("../pages/HomePage"));
+
 const Router = () => {
     return (
         <BrowserRouter>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-
+                    <Route path="/" element={<HomePage />} />
                 </Routes>
             </Suspense>
         </BrowserRouter>
